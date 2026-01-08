ca1187c5748d70dd4a24003403a7c673c854f48f
StarAppeal <silkfan11@gmail.com>
2026-01-08 21:12:37 +0100

    Add Spring profile annotation to PersonMongoRepository

--- src/main/java/de/assecor/persons/repository/PersonMongoRepository.java
+++ src/main/java/de/assecor/persons/repository/PersonMongoRepository.java
@@ -4,10 +4,13 @@ import de.assecor.persons.model.Color;
 import de.assecor.persons.model.document.PersonDocument;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Profile("mongo")
 public interface PersonMongoRepository extends MongoRepository<PersonDocument, Integer> {
 
   Optional<PersonDocument> findTopByOrderByIdDesc();
